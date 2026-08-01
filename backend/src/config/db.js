b0b4b3f1f086a0f8d9c0397df518d8fca27b26f1
Rahul Jha <rahuljhads556@gmail.com>
2026-08-01 13:39:12 +0530

    database setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'rahuljhads556@###', 
-    database: 'nexabridge_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
